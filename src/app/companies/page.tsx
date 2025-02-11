5de2b5858a897d1c6a94c91c0a3ed3bd2385f18a
Sergey Serdjukow <serduykovs@yandex.ru>
2025-02-11 07:08:36 +0100

    fix:

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { useAppStore } from "@/store/appStore"
 import Link from "next/link"
