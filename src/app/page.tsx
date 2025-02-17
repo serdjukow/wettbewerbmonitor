63aaac0f6d1ff81d927c17f383a95aae7b8be24a
Sergey Serdjukow <serduykovs@yandex.ru>
2025-02-17 08:59:11 +0100

    Full update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
-export default function Home(props: { disableCustomTheme?: boolean }) {
+export default function Home() {
     const disableCustomTheme = false
 
     return (
